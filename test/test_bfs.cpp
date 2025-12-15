3a07d0b477a6665ef997e89adc1e89eb9f950134
Kamil Salakhov <kasalakhov@avito.ru>
2025-12-15 13:29:06 +0300

    Added more tests for sequence bfs

--- test/test_bfs.cpp
+++ test/test_bfs.cpp
@@ -52,3 +52,78 @@ TEST(SequenceBfs, CycleGraph) {
 
     EXPECT_EQ(result, expected);
 }
+
+TEST(SequenceBfs, SingleNodeGraph) {
+    graph g(1);
+    const std::vector<int> result = seq_bfs(g, 0);
+    EXPECT_EQ(result, std::vector<int>{0});
+}
+
+TEST(SequenceBfs, DisconnectedGraph) {
+    graph g(4);
+    g[0] = {1};
+    g[1] = {0};
+    g[2] = {3};
+    g[3] = {2};
+    
+    const std::vector<int> expected = {0, 1, -1, -1};
+    const std::vector<int> result = seq_bfs(g, 0);
+    EXPECT_EQ(result, expected);
+}
+
+TEST(SequenceBfs, MultipleShortestPaths) {
+    graph g(5);
+    g[0] = {1, 2};
+    g[1] = {0, 3, 4};
+    g[2] = {0, 3};
+    g[3] = {1, 2, 4};
+    g[4] = {1, 3};
+    
+    const std::vector<int> expected = {0, 1, 1, 2, 2};
+    const std::vector<int> result = seq_bfs(g, 0);
+    EXPECT_EQ(result, expected);
+}
+
+TEST(SequenceBfs, SemiRandomGraph) {
+    /*
+       Структура графа:
+          0
+         / \
+        1   2
+       / \   \
+      3   4   5
+           \ /
+            6
+           / \
+          7   8
+    */
+    graph g(9);
+    g[0] = {1, 2};
+    g[1] = {0, 3, 4};
+    g[2] = {0, 5};
+    g[3] = {1};
+    g[4] = {1, 6};
+    g[5] = {2, 6};
+    g[6] = {4, 5, 7, 8};
+    g[7] = {6};
+    g[8] = {6};
+    
+    const std::vector<int> expected = {0, 1, 1, 2, 2, 2, 3, 4, 4};
+    const std::vector<int> result = seq_bfs(g, 0);
+    EXPECT_EQ(result, expected);
+}
+
+TEST(SequenceBfs, LargeStarGraph) {
+    const int n = 10000;
+    graph g(n);
+    for (int i = 1; i < n; ++i) {
+        g[0].push_back(i);
+        g[i].push_back(0);
+    }
+    
+    std::vector<int> expected(n, 1);
+    expected[0] = 0;
+    
+    const std::vector<int> result = seq_bfs(g, 0);
+    EXPECT_EQ(result, expected);
+}
