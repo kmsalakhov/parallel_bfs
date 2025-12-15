af750e83d7ef426ecd35da8fc965ff3fb4151bc4
Kamil Salakhov <kasalakhov@avito.ru>
2025-12-15 15:57:34 +0300

    Implemented parallel bfs

--- bfslib/bfs.cpp
+++ bfslib/bfs.cpp
@@ -1,5 +1,12 @@
 #include <queue>
 #include <cassert>
+#include <atomic>
+#include <algorithm>
+
+#include "parlay/primitives.h"
+#include "parlay/parallel.h"
+#include "parlay/sequence.h"
+#include "parlay/utilities.h"
 
 #include "bfs.h"
 
@@ -32,6 +39,56 @@ std::vector<int> seq_bfs(const graph& g, const int root) {
 }
 
 std::vector<int> par_bfs(const graph& g, const int root) {
-    // TODO(kasalakhov): changed to parallel
-    return seq_bfs(g, root);
+    const int n = g.size();
+    if (n == 0) {
+        // TODO(kasalakhov): no new struct
+        return std::vector<int>();
+    }
+
+    std::vector<std::atomic<int>> dist(n);
+    for (int i = 0; i < n; ++i) {
+        dist[i].store(-1);
+    }
+
+    std::vector<parlay::sequence<int>> f;
+    f.push_back({root});
+    dist[root].store(0);
+
+    for (int i = 0; !f[i].empty(); ++i) {
+        // TODO(kasalakhov): optimize memory usage
+        parlay::sequence<int> degs(f[i].size());
+        parlay::parallel_for(0, f[i].size(), [i, &f, &g, &degs](const size_t j) {
+            degs[j] = g[f[i][j]].size();
+        });
+        const int degs_sum = parlay::scan_inplace(degs);
+
+        parlay::sequence<int> new_f(degs_sum, -1);
+        // TODO(kasalakhov): as_const references?
+        parlay::parallel_for(0, f[i].size(), [i, &g, &f, &degs, &dist, &new_f](const size_t j) {
+            const int new_dist = dist[f[i][j]].load() + 1;
+            assert(new_dist != 0);
+
+            int expected = -1;
+            // TODO(kasalakhov): parallel for?
+            for (int k = 0; k < g[f[i][j]].size(); ++k) {
+                const int to = g[f[i][j]][k];
+                if (dist[to].compare_exchange_strong(expected, new_dist)) {
+                    new_f[degs[j] + k] = to;
+                }
+            }
+        });
+
+        parlay::filter(new_f, [](const int x) {
+            return x != -1;
+        });
+
+        f.push_back(new_f);
+    }
+
+    std::vector<int> result_dist(n);
+    for (int i = 0; i < n; ++i) {
+        result_dist[i] = dist[i].load();
+    }
+
+    return result_dist;
 }
